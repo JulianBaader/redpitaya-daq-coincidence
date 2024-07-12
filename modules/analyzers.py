afe4628fac2b7dd1107be381f1d0249bda0a4fa3
julian <urerc@student.kit.edu>
2024-07-12 10:58:09 +0200

    initial commit

---
+++ modules/analyzers.py
@@ -0,0 +1,72 @@
+from scipy import signal
+import numpy as np
+
+def tag_peaks(input_data, peak_config):
+    peaks = {}
+    peaks_prop = {}
+    for key in input_data.dtype.names:
+        peaks[key], peaks_prop[key] = signal.find_peaks(
+            input_data[key], prominence=peak_config['prominence'], distance=peak_config['distance'], width=peak_config['width']
+        )
+    return peaks, peaks_prop
+
+# Pulse height detection like Pavels algorhythm
+def find_rightmost_value_before_index(arr, i, min_value):
+    """
+    Find the rightmost value in the array `arr` that is less than or equal to `min_value`
+    before the given index `i`.
+
+    Args:
+        arr (array-like): The input array.
+        i (int): The index to search before.
+        min_value (float): The minimum value to compare against.
+
+    Returns:
+        int: The index of the rightmost value that satisfies the condition, or -1 if no such value is found.
+    """
+    
+    if i < 0 or i > len(arr):
+        return -1  # return -1 if i is out of the array bounds
+    
+    # Convert to numpy array if not already
+    arr = np.array(arr)
+    
+    # Get indices where the value is less than or equal to min_value
+    valid_indices = np.where(arr[:i] <= min_value)[0]
+    
+    if valid_indices.size == 0:
+        return -1  # No values less than or equal to min_value found before index i
+    
+    return valid_indices[-1]
+
+
+def pulse_height_pavel(input_data, pulse_height_pavel_config):
+    peaks, peaks_prop = tag_peaks(input_data, pulse_height_pavel_config['peak_config'])
+    for key in input_data.dtype.names:
+        #if len(peaks[key])==0:
+        #    return None, None
+        heights = []
+        #start_positions = []
+        for peak, left_ips in zip(peaks[key], peaks_prop[key]['left_ips']):
+            if pulse_height_pavel_config['baseline_mode'][key] == "gradient":
+                start_position = find_rightmost_value_before_index(np.gradient(input_data[key]), int(left_ips), pulse_height_pavel_config['baseline_value'][key])
+                if start_position != -1:
+                    heights.append(int(input_data[key][peak] - input_data[key][start_position]))
+                    #start_positions.append(start_position)
+                else: return None, None
+            elif pulse_height_pavel_config['baseline_mode'][key] == "constant":
+                heights.append(int(input_data[key][peak]-pulse_height_pavel_config['baseline_value'][key]))
+            elif pulse_height_pavel_config['baseline_mode'][key] == "testing_undershoot":
+                heights.append(np.min(input_data[key]))
+            elif pulse_height_pavel_config['baseline_mode'][key] == "average_pulse":
+                # https://asd.gsfc.nasa.gov/archive/astroe/eng/pha.html
+                print("Average Pulse Height not yet Implemented")
+            else:
+                print("No configuration for pulse_height_pavel[baseline_mode] given")
+        peaks_prop[key]['height'] = heights
+        #peaks_prop[key]['start'] = start_positions
+    
+    return peaks, peaks_prop
+        
+# <--- End Pulse height detection like Pavel
+
--- modules/filters.py
+++ modules/filters.py
@@ -86,6 +86,49 @@ def match_signature(peak_matrix, signature):
         match = match & (np.all(first) & np.all(second))
     return match
 
+def clipping(input_data, clipping_config):
+    """
+    Check if any channel in the input data exceeds the clipping level.
+
+
+    Currently clipping_config is a float, can be later eddited to be a dict if more requirements are neccesary
+    Args:
+        input_data (ndarray): The input data to check for clipping.
+        clipping_level (float): The clipping level to check against.
+
+    Returns:
+        ndarray:  returns the input_data.
+    """
+    for channel in input_data.dtype.names:
+        if np.max(input_data[channel]) >= clipping_config or np.min(input_data[channel]) <= -clipping_config:
+            return None
+    return input_data
+
+def coincidence(peaks, coincidence_config, trigger_channel):
+    """
+    Check if any channel has a peak within the specified coincidence window.
+
+    Args:
+        peaks (dict): A dictionary containing the peak data for each channel.
+        coincidence_window (int): The maximum distance between peaks for them to be considered coincident.
+        trigger_channel (str): The channel to use as the trigger.
+
+    Returns:
+        peaks or None: peaks if any channel has a peak within the coincidence window, None otherwise.
+    """
+    if len(peaks) == 0:
+        return None
+    for key in peaks.keys():
+        if key != trigger_channel and abs(peaks[key][0] - peaks[trigger_channel][0]+coincidence_config['offset']) <= coincidence_config['width']:
+            return peaks
+    return None
+
+
+def range_filter(input_data, range_config):
+    """
+        bin unsischer mit der Implementierung. Pulse Height Analysis wäre vlt etwas übertrieben besonders, wenn die vermutlich im nächsten Schritt gleich kommt
+        aber nur über max zu gehen ist auch blöd weil dann der offset von 0 fehlt
+    """
 
 if __name__ == "__main__":
     print("Script: " + os.path.basename(sys.argv[0]))
