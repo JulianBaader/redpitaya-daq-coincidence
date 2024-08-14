c54db601e635fc6962179bd3c06ac3d0755608ee
julian <urerc@student.kit.edu>
2024-08-14 19:19:26 +0200

    added mimocorb and npy. mimocorb not yet tested

--- rpDAQ_control.py
+++ rpDAQ_control.py
@@ -4,6 +4,12 @@ import sys
 import time
 import socket
 import matplotlib.pyplot as plt
+import argparse
+
+from mimocorb.activity_logger import Gen_logger
+from mimocorb import mimo_buffer as bm
+
+from npy_append_array import NpyAppendArray
 
 
 
@@ -52,37 +58,34 @@ ADC_RANGE = 4095 #TODO prob -4095 bis 4096 oder so ach keine Ahnung
 GENERATOR_BINS = 4096
 DISTRIBUTIONS = {'uniform': 0, 'poisson': 1}
 
-np.set_printoptions(threshold=1000)
-
 generator_array = np.zeros(GENERATOR_BINS, np.uint32)
 for i in range(16):  # initialize with delta-functions
     generator_array[(i + 1) * 256 - 1] = 1
 
         
-class rpControll:
-    def __init__(self ,config_dict = {}, callback = None):
+class rpControl:
+    """
+    TODO
+    """
+    def __init__(self ,config_dict = {}):
         self.load_config_from_dict(config_dict)
         self.check_config()
         
-        if callback is not None:
-            self.callback = callback
-        else:
-            self.callback = self.standard_callback
-            
-
+        
         # oscilloscope buffer
         self.osc_buffer = np.zeros(2 * self.total_samples, np.int16)
         self.osc_view = self.osc_buffer.view(np.uint8)
+        self.osc_reshaped = self.osc_buffer.reshape((2, self.total_samples), order='F') # order='F' is so that [1,2,3,4,5,6] -> [[1,3,5], [2,4,6]] which is the way the data is sent
         self.OSC_SIZE = self.osc_view.size
+        #TODO das muss dann anders gemacht werden
+        
+        self.event_count = 0
         
         # status buffer
         self.status_buffer = np.zeros(9, np.uint32)
         self.status_view = self.status_buffer.view(np.uint8)
         self.STATUS_SIZE = self.status_view.size
-        
-        # daq
 
-        
         # socket
         self.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
         self.socket.connect((self.ip, self.port))
@@ -93,12 +96,10 @@ class rpControll:
         self.setup_generator() #TODO test if überhaupt gewünscht
         self.setup_oscilloscope()
         
-        print("Setup done, starting Osci")
-        self.start_first_osc()
-
-        #self.test_rates()
-        self.main_loop()
         
+        self.start_first_osc()
+        print("Setup done")
+
         
     def command(self, code, number, value):
         self.socket.sendall(struct.pack("<Q", code << 56 | number << 52 | (int(value) & 0xFFFFFFFFFFFFF)))
@@ -113,28 +114,6 @@ class rpControll:
     def recv_osc_into_buffer(self, buffer):
         pass 
         
-    def test_rates(self):
-        input_rates = [r for r in range(100,10001,100)]
-        output_rates = []
-        ratio = []
-        for rate in input_rates:
-            self.command(25, 0, rate)
-            time.sleep(1)
-            out = self.main_loop()
-            output_rates.append(out)
-            print("Input rate: " + str(rate) + " Hz, Output rate: " + str(out) + " Hz" + ", Ratio: " + str(out / rate))
-            ratio.append(out / rate)
-        np.save("output_rates.npy", output_rates)
-        np.save("input_rates.npy", input_rates)
-        np.save("ratio.npy", ratio)
-        plt.plot(input_rates, ratio)
-        plt.show()
-        plt.plot(input_rates, output_rates)
-        plt.show()
-    
-    
-        
-    
     def main_loop(self): #TODO optimize
         start = time.time()
         event_counter = self.total_events
@@ -148,42 +127,12 @@ class rpControll:
         rate = self.total_events / (stop - start)
         return rate
         
-    def drain_daq(self):
-        self.command(32, 0, 0)
-        try:
-            while True:
-                self.socket.recv(1) #TODO ist das die beste Taktik?
-        except socket.timeout:
-            pass
+    #TODO Drain und del
         
-    def __del__(self): #TODO implement on server side
-        self.drain_daq()
-        self.socket.close()
-        
-        
-        
- 
-
     def start_first_osc(self):
         self.command(2, 0, 0)
         self.command(19, 0, 0)
-        # while not self.test_for_trigger():
-        #     time.sleep(1)
-        #     pass
         
-    def recv_status(self):
-        data = bytearray()
-        while len(data) < self.STATUS_SIZE:
-            data += self.socket.recv(self.STATUS_SIZE - len(data))
-        return data
-    
-    def test_for_trigger(self):
-        self.command(11, 0, 0)
-        self.status_view = np.frombuffer(self.recv_status(), np.uint8)
-        print(self.status_buffer)
-        return self.status_buffer[8] % 2 == 1
-        
-    
     def standard_callback(self, data):
         plt.plot(data[0])
         plt.show()
@@ -218,7 +167,116 @@ class rpControll:
         
     # <- Functions for setting up the RedPitaya
     
-    # -> Functions for reading the config dict
+    # -> Functions for interaction with mimoCoRB
+    
+    def setup_mimoCoRB(self, sink_list=None, config_dict=None, ufunc=None, **rb_info):
+        # basically the __init__ function of mimocorb.buffer_control.rbPut
+        
+        # sub-logger for this class
+        self.logger = Gen_logger(__class__.__name__)
+
+        # general part for each function (template)
+        if sink_list is None:
+            self.logger.error("Faulty ring buffer configuration passed, 'sink_list' missing!")
+            raise ValueError("ERROR! Faulty ring buffer configuration passed ('sink_list' missing)!")
+
+        self.sink = None
+        for key, value in rb_info.items():
+            if value == "read":
+                self.logger.error("Reading buffers not foreseen!!")
+                raise ValueError("ERROR! reading buffers not foreseen!!")
+            elif value == "write":
+                self.logger.info(f"Writing to buffer {sink_list[0]}")
+                self.sink = bm.Writer(sink_list[0])
+                if len(sink_list) > 1:
+                    self.logger.error("More than one sink presently not foreseen!")
+                    print("!!! More than one sink presently not foreseen!!")
+            elif value == "observe":
+                self.logger.error("Observer processes not foreseen!")
+                raise ValueError("ERROR! obervers not foreseen!!")
+
+        if self.sink is None:
+            self.logger.error("Faulty ring buffer configuration passed. No sink found!")
+            raise ValueError("Faulty ring buffer configuration passed. No sink found!")
+
+        
+        self.T_last = time.time()
+        
+    def osc_to_mimocorb(self):
+        if self.sink._active.is_set():
+            # do not write data if in paused mode
+            if self.sink._paused.is_set():
+                time.sleep(0.1)
+                return
+            T_data_ready = time.time()
+            timestamp = time.time_ns() * 1e-9  # in s as type float64
+            
+            
+            # get new buffer and store event data and meta-data
+            buffer = self.sink.get_new_buffer()
+            buffer.view(np.uint8)
+            # fill data
+            bytes_received = 0
+            while bytes_received < self.OSC_SIZE:
+                bytes_received += self.socket.recv_into(buffer[bytes_received:])
+            
+            self.event_count += 1
+            
+            # calculate deadtime
+            T_buffer_ready = time.time()
+            deadtime = T_buffer_ready - T_data_ready
+            deadtime_fraction = deadtime / (T_buffer_ready - self.T_last)
+            
+            # set metadata
+            self.sink.set_metadata(self.event_count, timestamp, deadtime_fraction)
+
+            self.T_last = T_buffer_ready
+        else:
+            # make sure last data entry is also processed
+            self.sink.process_buffer()
+            
+    def run_mimo_daq(self):
+        start = time.time()
+        while self.total_events - self.event_count > self.events_per_loop:
+            self.command(31, 0, self.events_per_loop)
+            for i in range(self.events_per_loop):
+                self.osc_to_mimocorb()
+        self.command(31, 0, self.total_events - self.event_count)
+        for i in range(self.total_events - self.event_count):
+            self.osc_to_mimocorb()
+        stop = time.time()
+        rate = self.total_events / (stop - start)
+        print("Data acquisition done. Rate: " + str(rate) + " Hz")
+            
+    # <- Functions for interaction with mimoCoRB
+    
+    # -> Functions for saving the data to file
+    
+    def osc_to_npy(self, filename):
+        bytes_received = 0
+        while bytes_received < self.OSC_SIZE:
+            bytes_received += self.socket.recv_into(self.osc_view[bytes_received:])
+        self.event_count += 1
+        with NpyAppendArray(filename) as npa:
+            npa.append(np.array([self.osc_reshaped]))
+
+    def run_and_save(self, filename):
+        start = time.time()
+        while self.total_events - self.event_count > self.events_per_loop:
+            self.command(31, 0, self.events_per_loop)
+            for i in range(self.events_per_loop):
+                self.osc_to_npy(filename)
+        self.command(31, 0, self.total_events - self.event_count)
+        for i in range(self.total_events - self.event_count):
+            self.osc_to_npy(filename)
+        stop = time.time()
+        rate = self.total_events / (stop - start)
+        print("Data acquisition done. Rate: " + str(rate) + " Hz")
+        
+    # <- Functions for saving the data to file
+        
+    
+    # -> Functions for reading the config
         
     def load_config_from_dict(self, config_dict):
         # general configuration
@@ -227,7 +285,7 @@ class rpControll:
         self.sample_rate = config_dict["sample_rate"] if "sample_rate" in config_dict else 4
         self.ch1_negated = config_dict["ch1_negated"] if "ch1_negated" in config_dict else False
         self.ch2_negated = config_dict["ch2_negated"] if "ch2_negated" in config_dict else False
-        self.total_events = config_dict["total_events"] if "total_events" in config_dict else 10
+        self.total_events = config_dict["total_events"] if "total_events" in config_dict else 10000
         self.events_per_loop = config_dict["events_per_loop"] if "events_per_loop" in config_dict else 1000
         
         # oscilloscope configuration
@@ -246,6 +304,32 @@ class rpControll:
         self.spectrum = config_dict["spectrum"] if "spectrum" in config_dict else generator_array #TODO das ist nicht so schön
         self.start_generator = config_dict["start_generator"] if "start_generator" in config_dict else True
         
+    def load_config_from_parser(self):
+        print("Not implemented yet. Use load_config_from_dict instead. Think of important parameters.")
+        # ip
+        
+    def config(self):
+        return {
+            "ip": self.ip,
+            "port": self.port,
+            "sample_rate": self.sample_rate,
+            "ch1_negated": self.ch1_negated,
+            "ch2_negated": self.ch2_negated,
+            "total_events": self.total_events,
+            "events_per_loop": self.events_per_loop,
+            "trigger_source": self.trigger_source,
+            "trigger_slope": self.trigger_slope,
+            "trigger_mode": self.trigger_mode,
+            "trigger_level": self.trigger_level,
+            "pretrigger_samples": self.pretrigger_samples,
+            "total_samples": self.total_samples,
+            "fall_time": self.fall_time,
+            "rise_time": self.rise_time,
+            "pulse_rate": self.pulse_rate,
+            "distribution": self.distribution,
+            "spectrum": self.spectrum,
+            "start_generator": self.start_generator
+        }
         
     def check_config(self):
         if self.sample_rate not in SAMPLE_RATES:
@@ -271,72 +355,15 @@ class rpControll:
         
     # <- Functions for reading the config dict
     
-        
 
+    
+def rp_mimocorb(source_list=None, sink_list=None, observe_list=None, config_dict=None, **rb_info):
+    control = rpControl(config_dict)
+    control.setup_mimoCoRB(config_dict=config_dict, sink_list=sink_list, **rb_info)
+    control.run_mimo_daq()
+    
 
-# class rp_mimocorb:
-#     """Interface for rpControll to the daq rinbuffer mimoCoRB"""
-
-#     def __init__(self, source_list=None, sink_list=None, observe_list=None, config_dict=None, **rb_info):
-#         # initialize mimoCoRB interface
-#         self.rb_exporter = rbPut(config_dict=config_dict, sink_list=sink_list, **rb_info)
-#         self.number_of_channels = len(self.rb_exporter.sink.dtype)
-#         self.events_required = 100000 if "eventcount" not in config_dict else config_dict["eventcount"]
-
-#         self.event_count = 0
-#         self.active = True
-
-#     def __call__(self, data):
-#         """function called by redPoscdaq"""
-#         if (self.events_required == 0 or self.event_count < self.events_required) and self.active:
-#             # deliver pulse data and no metadata
-#             self.active = self.rb_exporter(data, None)  # send data
-#             self.event_count += 1
-#         else:
-#             self.active = self.rb_exporter(None, None)  # send None when done
-#             print("redPoscdaq exiting")
-#             sys.exit()
-            
-# def rp_to_rb(source_list=None, sink_list=None, observe_list=None, config_dict=None, **rb_info):
-#     """Main function,
-#     executed as a multiprocessing Process, to pass data from the RedPitaya to a mimoCoRB buffer
-
-#     :param config_dict: configuration dictionary
-#       - events_required: number of events to be taken
-#       - number_of_samples, sample_time_ns, pretrigger_samples and analogue_offset
-#       - decimation index, invert flags, trigger mode and trigger direction for RedPitaya
-#     """
-
-#     # initialize mimocorb class
-#     rb_source = rp_mimocorb(config_dict=config_dict, sink_list=sink_list, **rb_info)
-#     rpControll(config_dict=config_dict, callback=rb_source)
-
-
-# if __name__ == "__main__":  # --------------------------------------
-#     # run mimoCoRB data acquisition suite
-#     # the code below is idenical to the mimoCoRB script run_daq.py
-#     import argparse
-#     import sys
-#     import time
-#     from mimocorb.buffer_control import run_mimoDAQ
-
-#     # define command line arguments ...
-#     parser = argparse.ArgumentParser(description=__doc__)
-#     parser.add_argument("filename", nargs="?", default="demo_setup.yaml", help="configuration file")
-#     parser.add_argument("-v", "--verbose", type=int, default=2, help="verbosity level (2)")
-#     parser.add_argument("-d", "--debug", action="store_true", help="switch on debug mode (False)")
-#     # ... and parse command line input
-#     args = parser.parse_args()
-
-#     print("\n*==* script " + sys.argv[0] + " running \n")
-#     daq = run_mimoDAQ(args.filename, verbose=args.verbose, debug=args.debug)
-#     daq.setup()
-#     daq.run()
-#     print("\n*==* script " + sys.argv[0] + " finished " + time.asctime() + "\n")
-
-
-def none_function(data):
-    pass
-
-rpControll({}, None)
+if __name__ == "__main__":
+    control = rpControl()
+    control.run_and_save("test.npy")
 
